7e8212973f63adfa878f24f05c34f4af99cf816d
rythmeur <rythmeur@gmail.com>
2014-03-08 12:03:08 +0400

    week1

--- week1/Lecture.py
+++ week1/Lecture.py
@@ -8,12 +8,12 @@ import pylab
 
 pylab.figure(1) #create figure 1
 pylab.plot([1,2,3,4], [1,2,3,4]) #draw on figure 1
-pylab.figure(2) #create figure 2
+# pylab.figure(2) #create figure 2
 pylab.plot([1,4,2,3], [5,6,7,8]) #draw on figure 2
-pylab.savefig('Figure-Eric') #save figure 2
+# pylab.savefig('Figure-Eric') #save figure 2
 pylab.figure(1) #go back to working on figure 1
 pylab.plot([5,6,10,3]) #draw again on figure 1
-pylab.savefig('Figure-Grimson') #save figure 1
+# pylab.savefig('Figure-Grimson') #save figure 1
 pylab.show()
 
 

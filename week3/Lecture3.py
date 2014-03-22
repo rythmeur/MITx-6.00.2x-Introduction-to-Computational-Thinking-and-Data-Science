e1e7db26ad6acba8012388a7cf68295db155e79d
rythmeur <rythmeur@gmail.com>
2014-03-23 02:38:58 +0400

    23-03-2014

--- week3/Lecture3.py
+++ week3/Lecture3.py
@@ -110,3 +110,144 @@ def main():
 
 
 
+
+####################################################################
+
+import random, pylab
+#set line width
+pylab.rcParams['lines.linewidth'] = 6
+#set font size for titles
+pylab.rcParams['axes.titlesize'] = 20
+#set font size for labels on axes
+pylab.rcParams['axes.labelsize'] = 20
+#set size of numbers on x-axis
+pylab.rcParams['xtick.major.size'] = 5
+#set size of numbers on y-axis
+pylab.rcParams['ytick.major.size'] = 5
+
+
+def flip(numFlips):
+    heads = 0
+    for i in range(numFlips):
+        if random.random() < 0.5:
+            heads += 1
+    return heads/float(numFlips)
+
+
+def flipPlot(minExp, maxExp):
+    """Assumes minExp and maxExp positive integers; minExp < maxExp
+       Plots results of 2**minExp to 2**maxExp coin flips"""
+    ratios = []
+    diffs = []
+    xAxis = []
+    for exp in range(minExp, maxExp + 1):
+        xAxis.append(2**exp)
+    for numFlips in xAxis:
+        numHeads = 0
+        for n in range(numFlips):
+            if random.random() < 0.5:
+                numHeads += 1
+        numTails = numFlips - numHeads
+        ratios.append(numHeads/float(numTails))
+        diffs.append(abs(numHeads - numTails))
+    pylab.title('Difference Between Heads and Tails')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('Abs(#Heads - #Tails)')
+    pylab.plot(xAxis, diffs)
+    pylab.figure()
+    pylab.title('Heads/Tails Ratios')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('Heads/Tails')
+    pylab.plot(xAxis, ratios)
+
+##random.seed(0)
+##flipPlot(4, 20)
+##pylab.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def flipPlot(minExp, maxExp):
+    """Assumes minExp and maxExp positive integers; minExp < maxExp
+       Plots results of 2**minExp to 2**maxExp coin flips"""
+    ratios = []
+    diffs = []
+    xAxis = []
+    for exp in range(minExp, maxExp + 1):
+        xAxis.append(2**exp)
+    for numFlips in xAxis:
+        numHeads = 0
+        for n in range(numFlips):
+            if random.random() < 0.5:
+                numHeads += 1
+        numTails = numFlips - numHeads
+        ratios.append(numHeads/float(numTails))
+        diffs.append(abs(numHeads - numTails))
+    pylab.title('Difference Between Heads and Tails')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('Abs(#Heads - #Tails)')
+    pylab.plot(xAxis, diffs)
+    pylab.figure()
+    pylab.title('Heads/Tails Ratios')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('Heads/Tails')
+    pylab.plot(xAxis, ratios)
+    #Additional code to produce different plots from the
+    #same data
+    pylab.figure()
+    pylab.title('Difference Between Heads and Tails')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('Abs(#Heads - #Tails)')
+    pylab.plot(xAxis, diffs, 'bo')
+    pylab.semilogx()
+    pylab.semilogy()
+    pylab.figure()
+    pylab.title('Heads/Tails Ratios')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('Heads/Tails')
+    pylab.plot(xAxis, ratios, 'bo')
+    pylab.semilogx()
+
+random.seed(0)
+flipPlot(4, 20)
+pylab.show()
+
+#############################################
+
+def stdDev(X):
+    mean = sum(X)/float(len(X))
+    tot = 0.0
+    for x in X:
+        tot += (x - mean)**2
+    return (tot/len(X))**0.5
+
+def stdDevOfLengths(L):
+    """
+    L: a list of strings
+
+    returns: float, the standard deviation of the lengths of the strings,
+      or NaN if L is empty.
+    """
+    list_x = []
+    if len (L) > 0:
+        for i in L:
+            list_x.append(len(i))
+        return stdDev(list_x)
+    return float ('NaN')
+
+L = ['apples', 'oranges', 'kiwis', 'pineapples']
+y = []
+print stdDevOfLengths(L)
+print stdDevOfLengths(y)
+
+

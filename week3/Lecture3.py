bc915a51e35b31e897432e12a9950ae95046e51f
rythmeur <rythmeur@gmail.com>
2014-03-22 23:13:48 +0400

    22-03-2014

--- week3/Lecture3.py
+++ week3/Lecture3.py
@@ -1,42 +1,112 @@
-__author__ = 'Intel'
+################################################################################
+#----------L3 PROBLEM 5
 import random
 
-def rollDie():
-    return random.choice([1,2,3,4,5,6])
+class intDict(object):
+    """A dictionary with integer keys"""
 
-def rollN(n):
-    result = ''
-    for i in range(n):
-        result = result + str(rollDie())
-    return result
+    def __init__(self, numBuckets):
+        """Create an empty dictionary"""
+        self.buckets = []
+        self.numBuckets = numBuckets
+        for i in range(numBuckets):
+            self.buckets.append([])
 
-print rollN(5)
+    def addEntry(self, dictKey, dictVal):
+        """Assumes dictKey an int.  Adds an entry."""
+        hashBucket = self.buckets[dictKey%self.numBuckets]
+        for i in range(len(hashBucket)):
+            if hashBucket[i][0] == dictKey:
+                hashBucket[i] = (dictKey, dictVal)
+                return
+        hashBucket.append((dictKey, dictVal))
 
-def getTarget(goal):
-    numTries = 0
-    numRolls = len(goal)
-    while True:
-        numTries += 1
-        result = rollN(numRolls)
-        if result == goal:
-            return numTries
+    def getValue(self, dictKey):
+        """Assumes dictKey an int.  Returns entry associated
+           with the key dictKey"""
+        hashBucket = self.buckets[dictKey%self.numBuckets]
+        for e in hashBucket:
+            if e[0] == dictKey:
+                return e[1]
+        return None
 
-def runSim(goal, numTrials):
-    total = 0
-    for i in range(numTrials):
-        total += getTarget(goal)
-    print 'Average number of tries =', total/float(numTrials)
+    def __str__(self):
+        res = '{'
+        for b in self.buckets:
+            for t in b:
+                res = res + str(t[0]) + ':' + str(t[1]) + ','
+        return res[:-1] + '}' #res[:-1] removes the last comma
 
-##runSim('11111', 100)
-##runSim('54324', 100)
 
-def atLeastOneOne(numRolls, numTrials):
-    numSuccess = 0
-    for i in range(numTrials):
-        rolls = rollN(numRolls)
-        if '1' in rolls:
-            numSuccess += 1
-        fracSuccess = numSuccess/float(numTrials)
-    print fracSuccess
+def collision_prob(numBuckets, numInsertions):
+    '''
+    Given the number of buckets and the number of items to insert,
+    calculates the probability of a collision.
+    '''
+    prob = 1.0
+    for i in range(1, numInsertions):
+        prob = prob * ((numBuckets - i) / float(numBuckets))
+    return 1 - prob
 
-atLeastOneOne(10, 1000)
+def task7_find_largest_classroom(probability):
+    prob = 1.0
+    numBuckets = 365
+    numInsertions = 1000
+    while probability < prob:
+        numInsertions-=1
+        prob = collision_prob(numBuckets, numInsertions)
+    return numInsertions
+print "task7", task7_find_largest_classroom(0.99)
+
+def sim_insertions(numBuckets, numInsertions):
+    '''
+    Run a simulation for numInsertions insertions into the hash table.
+    '''
+    choices = range(numBuckets)
+    used = []
+    for i in range(numInsertions):
+        hashVal = random.choice(choices)
+        if hashVal in used:
+            return False
+        else:
+            used.append(hashVal)
+    return True
+
+
+def observe_prob(numBuckets, numInsertions, numTrials):
+    '''
+    Given the number of buckets and the number of items to insert,
+    runs a simulation numTrials times to observe the probability of a collision.
+    '''
+    probs = []
+    for t in range(numTrials):
+        probs.append(sim_insertions(numBuckets, numInsertions))
+    return 1 - sum(probs)/float(numTrials)
+print observe_prob(1000, 200, 1000)
+
+def main():
+    hash_table = intDict(25)
+    hash_table.addEntry(15, 'a')
+#    random.seed(1) # Uncomment for consistent results
+    for i in range(20):
+       hash_table.addEntry(int(random.random() * (10 ** 9)), i)
+    hash_table.addEntry(15, 'b')
+    print hash_table.buckets  #evil
+    print '\n', 'hash_table =', hash_table
+    print hash_table.getValue(15)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

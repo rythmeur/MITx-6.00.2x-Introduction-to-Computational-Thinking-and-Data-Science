8ff8fa206d5edf138296bee11c15095526aff086
rythmeur <rythmeur@gmail.com>
2014-04-06 00:30:18 +0400

    06-04-2014

--- week4/Lecture4.py
+++ week4/Lecture4.py
@@ -20,20 +20,6 @@ def checkPascal(numTrials, roll):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def rollLoadedDie():
     if random.random() < 1.0/5.5:
         return 6
@@ -41,20 +27,6 @@ def rollLoadedDie():
         return random.choice([1,2,3,4,5])
 
 ##checkPascal(10000, rollLoadedDie)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 def flip(numFlips):
@@ -88,4 +60,163 @@ def noReplacementSimulation(numTrials):
             event_occured+=1.
     return event_occured/numTrials
 
-print noReplacementSimulation(1000)
+print noReplacementSimulation(100)
+print "-------------------------------------------------------"
+import random, pylab
+
+#set line width
+pylab.rcParams['lines.linewidth'] = 6
+#set font size for titles
+pylab.rcParams['axes.titlesize'] = 20
+#set font size for labels on axes
+pylab.rcParams['axes.labelsize'] = 20
+#set size of numbers on x-axis
+pylab.rcParams['xtick.major.size'] = 5
+#set size of numbers on y-axis
+pylab.rcParams['ytick.major.size'] = 5
+#set font size for text
+pylab.rcParams['legend.fontsize'] = 20
+
+def stdDev(X):
+    mean = sum(X)/float(len(X))
+    tot = 0.0
+    for x in X:
+        tot += (x - mean)**2
+    return (tot/len(X))**0.5
+
+def throwNeedles(numNeedles):
+    inCircle = 0
+    for Needles in xrange(1, numNeedles + 1, 1):
+        x = random.random()
+        y = random.random()
+        if (x*x + y*y)**0.5 <= 1.0:
+            inCircle += 1
+    return 2*(inCircle/float(numNeedles))
+
+def getEst(numNeedles, numTrials):
+    estimates = []
+    for t in range(numTrials):
+        piGuess = throwNeedles(numNeedles)
+        estimates.append(piGuess)
+    sDev = stdDev(estimates)
+    curEst = sum(estimates)/len(estimates)
+    print 'Est. = ' + str(curEst) +\
+          ', Std. dev. = ' + str(round(sDev, 6))\
+          + ', Needles = ' + str(numNeedles)
+    return (curEst, sDev)
+
+def estPi(precision, numTrials):
+    numNeedles = 1000
+    sDev = precision
+    while sDev >= precision/2.0:
+        curEst, sDev = getEst(numNeedles, numTrials)
+        numNeedles *= 2
+    return curEst
+
+random.seed(0)
+estPi(0.05, 100)
+
+print "_________________________________________________________________"
+
+import random, pylab
+
+#set line width
+pylab.rcParams['lines.linewidth'] = 6
+#set font size for titles
+pylab.rcParams['axes.titlesize'] = 20
+#set font size for labels on axes
+pylab.rcParams['axes.labelsize'] = 20
+#set size of numbers on x-axis
+pylab.rcParams['xtick.major.size'] = 5
+#set size of numbers on y-axis
+pylab.rcParams['ytick.major.size'] = 5
+
+
+
+##def makeNormal(mean, sd, numSamples):
+##    samples = []
+##    for i in range(numSamples):
+##        samples.append(random.gauss(mean, sd))
+##    pylab.hist(samples, bins = 101)
+##
+##makeNormal(0, 1.0, 100000)
+##pylab.show()
+
+
+
+
+
+def clear(n, clearProb, steps):
+    numRemaining = [n]
+    for t in range(steps):
+        numRemaining.append(n*((1-clearProb)**t))
+    pylab.plot(numRemaining, label = 'Exponential Decay')
+
+##clear(1000, 0.01, 500)
+##pylab.xlabel('Number of Steps')
+##pylab.ylabel('Number of Molecules')
+##pylab.show()
+
+
+def clearSim(n, clearProb, steps):
+    numRemaining = [n]
+    for t in range(steps):
+        numLeft = numRemaining[-1]
+        for m in range(numRemaining[-1]):
+            if random.random() <= clearProb:
+                numLeft -= 1
+        numRemaining.append(numLeft)
+    pylab.plot(numRemaining, 'ro', label = 'Simulation')
+
+clear(10000, 0.01, 1000)
+clearSim(10000, 0.01, 1000)
+pylab.xlabel('Number of Steps')
+pylab.ylabel('Number of Molecules')
+pylab.legend()
+pylab.show()
+
+print "______________________________________________________________"
+import random, pylab
+
+#set line width
+pylab.rcParams['lines.linewidth'] = 6
+#set font size for titles
+pylab.rcParams['axes.titlesize'] = 20
+#set font size for labels on axes
+pylab.rcParams['axes.labelsize'] = 20
+#set size of numbers on x-axis
+pylab.rcParams['xtick.major.size'] = 5
+#set size of numbers on y-axis
+pylab.rcParams['ytick.major.size'] = 5
+
+
+def clear(n, clearProb, steps):
+    numRemaining = [n]
+    for t in range(steps):
+        numRemaining.append(n*((1-clearProb)**t))
+    pylab.plot(numRemaining, label = 'Exponential Decay')
+
+##clear(1000, 0.01, 500)
+##pylab.xlabel('Number of Steps')
+##pylab.ylabel('Number of Molecules')
+##pylab.title('Clearance of Molecules')
+##pylab.show()
+
+
+
+def clearSim(n, clearProb, steps):
+    numRemaining = [n]
+    for t in range(steps):
+        numLeft = numRemaining[-1]
+        for m in range(numRemaining[-1]):
+            if random.random() <= clearProb:
+                numLeft -= 1
+        numRemaining.append(numLeft)
+    pylab.plot(numRemaining, 'ro', label = 'Simulation')
+
+clear(10000, 0.01, 1000)
+clearSim(10000, 0.01, 1000)
+pylab.xlabel('Number of Steps')
+pylab.ylabel('Number of Molecules')
+pylab.legend()
+pylab.show()

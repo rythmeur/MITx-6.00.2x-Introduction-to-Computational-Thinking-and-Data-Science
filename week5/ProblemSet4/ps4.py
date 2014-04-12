2244cfbe9b95c126796dae7bcd7344dd6fa09b22
rythmeur <rythmeur@gmail.com>
2014-04-13 03:04:07 +0400

    13-04-2014

--- week5/ProblemSet4/ps4.py
+++ week5/ProblemSet4/ps4.py
@@ -23,10 +23,54 @@ def simulationDelayedTreatment(numTrials):
     """
     
     # TODO
+    result = {300:[], 150:[], 75:[], 0:[]}
+    # result = {3:[], 2:[], 1:[], 0:[]}
+    for key in result:
+        print "key = ", key
+        for i in range(numTrials):
+            numViruses = 100
+            # numViruses = 10
+            maxPop = 1000
+            maxBirthProb = 0.1
+            clearProb = 0.05
+            resistances = {'guttagonol': False}
+            mutProb = 0.005
+            viruses = [];
+            for i in range(numViruses):
+                virus = ResistantVirus(maxBirthProb, clearProb, resistances, mutProb);
+                viruses.append(virus);
 
+            patient = TreatedPatient(viruses, maxPop);
+            trial = []
+            for step in range(key):
+                virus_population_after_update = patient.update()
+                trial.append(virus_population_after_update)
+            # result[key].append(trial)
 
+            patient.addPrescription('guttagonol')
+            # for step in range(150):
+            for step in range(150):
+                virus_population_after_update = patient.update()
+                trial.append(virus_population_after_update)
+            result[key].append(trial)
 
+    ## mean for [[1,2,3], [2,3,4], [3,4,5]] = [2,3,4]
+    # for key in result:
+    #     data = numpy.array (result[key])
+    #     # print numpy.average(data,0) #0 - by coloms, inside row
+    #     result[key] = list(numpy.average(data,0))
+    # print result
 
+    for i,key in enumerate(result):
+        data = numpy.array (result[key])
+        array_final_steps = data.take(-1,1) # 1 - by rows, inside column
+        # print "array_final_steps", array_final_steps
+        pylab.subplot(4,1,i)
+        pylab.title(str(key))
+        pylab.hist(array_final_steps,bins=20)
+    pylab.show()
+
+# simulationDelayedTreatment(100)
 
 
 #
@@ -47,15 +91,57 @@ def simulationTwoDrugsDelayedTreatment(numTrials):
     """
     # TODO
 
-numViruses = 100
-maxPop = 1000
-maxBirthProb = 0.1
-clearProb = 0.05
-resistances = {'guttagonol': False}
-mutProb = 0.05
-viruses = [];
-for i in range(numViruses):
-    virus = ResistantVirus(maxBirthProb, clearProb, resistances, mutProb);
-    viruses.append(virus);
+    result = {300:[], 150:[], 75:[], 0:[]}
+    # result = {3:[], 2:[], 1:[], 0:[]}
+    for key in result:
+        print "key = ", key
+        for i in range(numTrials):
+            numViruses = 100
+            # numViruses = 10
+            maxPop = 1000
+            maxBirthProb = 0.1
+            clearProb = 0.05
+            resistances = {'guttagonol': False, 'grimpex': False}
+            mutProb = 0.005
+            viruses = [];
+            for i in range(numViruses):
+                virus = ResistantVirus(maxBirthProb, clearProb, resistances, mutProb);
+                viruses.append(virus);
 
-patient = TreatedPatient(viruses, maxPop);
+            patient = TreatedPatient(viruses, maxPop);
+            trial = []
+
+            for step in range(150):
+                virus_population_after_update = patient.update()
+                trial.append(virus_population_after_update)
+
+            patient.addPrescription('guttagonol')
+            for step in range(key):
+                virus_population_after_update = patient.update()
+                trial.append(virus_population_after_update)
+            # result[key].append(trial)
+
+            patient.addPrescription('grimpex')
+            # for step in range(150):
+            for step in range(150):
+                virus_population_after_update = patient.update()
+                trial.append(virus_population_after_update)
+            result[key].append(trial)
+
+    ## mean for [[1,2,3], [2,3,4], [3,4,5]] = [2,3,4]
+    # for key in result:
+    #     data = numpy.array (result[key])
+    #     # print numpy.average(data,0) #0 - by coloms, inside row
+    #     result[key] = list(numpy.average(data,0))
+    # print result
+
+    for i,key in enumerate(result):
+        data = numpy.array (result[key])
+        array_final_steps = data.take(-1,1) # 1 - by rows, inside column
+        # print "array_final_steps", array_final_steps
+        pylab.subplot(4,1,i)
+        pylab.title(str(key))
+        pylab.hist(array_final_steps,bins=20)
+    pylab.show()
+
+simulationTwoDrugsDelayedTreatment(100)
